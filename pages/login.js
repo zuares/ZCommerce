cbbf270a64d16663f9839c81de845e3c3e7b6fdc
Zuares <zuaresquad@gmail.com>
2021-01-14 09:23:04 +0700

    Membuat layouting halaman login

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function login() {
+    return (
+        <div>
+            <div>
+                Gambar
+            </div>
+            <div>
+                Form Login
+            </div>
+        </div>
+    );
+}
+
+export default login;
